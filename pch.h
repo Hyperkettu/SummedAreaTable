f9f967190f0a63e6a6e9cb1cf6f114227e5fc877
Olli Kettunen <oj.kettunen@gmail.com>
2024-10-29 08:04:47 +0200

    First solution

---
+++ pch.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <vulkan/vulkan.h>
+#include <vector>
+#include <iostream>
+#include <stdexcept>
+#include <cstring>
+#include <fstream>
+#include <array>
+
---
+++ shaders/compile.bat
@@ -0,0 +1,2 @@
+C:\VulkanSDK/1.3.275.0/Bin/glslc.exe sat.comp -o sat.spv
+pause
